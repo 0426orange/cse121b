60d387e33baf11e0374b4db1d8275133c78453c5
0426orange <114186672+0426orange@users.noreply.github.com>
2023-10-07 18:06:08 -0600

    Add files via upload

---
+++ cse121b/scripts/w03-task.js
@@ -0,0 +1,77 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Declaration - Add Numbers */
+function add(number1, number2) {
+    return number1 + number2;
+  }
+  
+  const addNumbers = function() {
+    let addNumber1 = Number(document.querySelector('#add1').value);
+    let addNumber2 = Number(document.querySelector('#add2').value);
+    document.querySelector('#sum').value = add(addNumber1, addNumber2);
+  };
+  
+  document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+  
+  /* Function Expression - Subtract Numbers */
+  const subtract = function(number1, number2) {
+    return number1 - number2;
+  };
+  
+  const subtractNumbers = function() {
+    let subtractNumber1 = Number(document.querySelector('#subtract1').value);
+    let subtractNumber2 = Number(document.querySelector('#subtract2').value);
+    document.querySelector('#difference').value = subtract(subtractNumber1, subtractNumber2);
+  };
+  
+  document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
+  
+  /* Arrow Function - Multiply Numbers */
+  const multiply = (factor1, factor2) => factor1 * factor2;
+  
+  const multiplyNumbers = () => {
+    let factor1 = Number(document.querySelector('#factor1').value);
+    let factor2 = Number(document.querySelector('#factor2').value);
+    document.querySelector('#product').value = multiply(factor1, factor2);
+  };
+  
+  document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
+  
+  /* Open Function Use - Divide Numbers */
+  function divide(dividend, divisor) {
+    return dividend / divisor;
+  }
+  
+  const divideNumbers = function() {
+    let dividend = Number(document.querySelector('#dividend').value);
+    let divisor = Number(document.querySelector('#divisor').value);
+    document.querySelector('#quotient').value = divide(dividend, divisor);
+  };
+  
+  document.querySelector('#divideNumbers').addEventListener('click', divideNumbers);
+  
+/* Decision Structure */
+const currentDate = new Date();
+let currentYear;
+currentYear = currentDate.getFullYear();
+document.getElementById("year").textContent = currentYear;
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+document.getElementById("array").textContent = numbersArray.join(', ');
+
+/* Output Odds Only Array */
+const oddNumbers = numbersArray.filter(number => number % 2 === 1);
+document.getElementById("odds").textContent = oddNumbers.join(', ');
+/* Output Evens Only Array */
+const evenNumbers = numbersArray.filter(number => number % 2 === 0);
+document.getElementById("evens").textContent = evenNumbers.join(', ');
+/* Output Sum of Org. Array */
+const sumOfArray = numbersArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+document.getElementById("sumOfArray").textContent = sumOfArray;
+
+/* Output Multiplied by 2 Array */
+const sumOfMultiplied = numbersArray.map(number => number * 2).reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+document.getElementById("sumOfMultiplied").textContent = sumOfMultiplied;
