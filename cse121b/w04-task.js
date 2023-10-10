e7de852d5c0891c33b2471088b07dae40fe07676
0426orange <114186672+0426orange@users.noreply.github.com>
2023-10-10 15:46:09 -0600

    Add files via upload

--- cse121b/w04-task.js
+++ cse121b/w04-task.js
@@ -1,57 +1,45 @@
 /* LESSON 3 - Programming Tasks */
 
-/* Profile Object  */
+/* Profile Object */
 let myProfile = {
-    name: "Tzu Hsuan Hsu"
+  name: "Tzu Hsuan Hsu",
+  favoriteFoods: ['Rice', 'Fried Chicken', 'Boba Milk'],
+  hobbies: ['Video Games', 'Music'],
+  placesLived: [
+      {
+          place: 'Rexburg, ID',
+          length: '1 year'
+      }
+  ]
 };
+
+/* Photo */
 let profilePicture = 'images/image.JPG';
-
-
-/* Populate Profile Object with placesLive objects */
-myProfile.placedLived.push(
-    {
-        pace:'Rexburg, ID',
-        length:'1 year'
-    }
-)
-
-
-
 
 /* DOM Manipulation - Output */
 
 /* Name */
 document.querySelector('#name').textContent = myProfile.name;
+
 /* Photo with attributes */
-document.getElementById("photo").src = myProfile.photo;
+document.getElementById("photo").src = profilePicture;
 document.getElementById("photo").alt = myProfile.name;
 
-
-/* Favorite Foods List*/
-
+/* Favorite Foods List */
 myProfile.favoriteFoods.forEach((food) => {
   let li = document.createElement("li");
   li.textContent = food;
   document.querySelector('#favorite-foods').appendChild(li);
 });
 
-favoriteFoods: [
-    'Rice','Fried chichken', 'Boba Milk'
-]
-
 /* Hobbies List */
-myProfile.favoriteFoods.forEach((hobbies) => {
-    let li = document.createElement("li");
-    li.textContent = hobbies;
-    document.querySelector('#hobbies').appendChild(li);
-  });
-  
-hobbies: ['Video Games','Music',''
+myProfile.hobbies.forEach((hobby) => {
+  let li = document.createElement("li");
+  li.textContent = hobby;
+  document.querySelector('#hobbies').appendChild(li);
+});
 
-]
-
-/* Places Lived DataList */
-placelived: []
+/* Places Lived Data List */
 const placesLivedList = document.getElementById("places-lived");
 
 myProfile.placesLived.forEach((place) => {
@@ -64,4 +52,3 @@ myProfile.placesLived.forEach((place) => {
   placesLivedList.appendChild(dt);
   placesLivedList.appendChild(dd);
 });
-
